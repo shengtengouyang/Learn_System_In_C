5a01cc72a7100a993b1bb2a2a7cbedfa8e7dc975
shengteng ouyang <shoouyang@cs.stonybrook.edu>
2020-04-24 23:33:14 -0400

    remove sigprocmask in child, seems impossible to detect sighup for specific loop

--- hw4/src/worker.c
+++ hw4/src/worker.c
@@ -23,24 +23,24 @@ int worker(void) {
     signal(SIGHUP, sighup_handler);
     signal(SIGTERM, sigterm_handler);
     debug("continue");
-    sigset_t mask, prev;
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGHUP);
-    sigprocmask(SIG_BLOCK, &mask, &prev);
+    // sigset_t mask, prev;
+    // sigemptyset(&mask);
+    // sigaddset(&mask, SIGHUP);
+    // sigprocmask(SIG_BLOCK, &mask, &prev);
     while(1){
         debug("Idling");
-        raise(SIGSTOP);
         canceledp=0;
-        sigprocmask(SIG_SETMASK, &prev, NULL);
+        raise(SIGSTOP);
+        // sigprocmask(SIG_SETMASK, &prev, NULL);
         struct problem * problems=malloc(sizeof(struct problem));
         debug("Reading problems");
         read(0, problems, sizeof(struct problem));
         size_t datasize=problems->size-sizeof(struct problem);
         problems=realloc(problems, problems->size);
-        read(0, (void *)problems+sizeof(struct problem), datasize);
+        read(0, problems->data, datasize);
         debug("got problem size: %ld, type: %d, variant: %d ", problems->size, problems->type, problems->var);
         struct result * out=solvers[problems->type].solve(problems, &canceledp);
-        sigprocmask(SIG_BLOCK, &mask, &prev);
+        // sigprocmask(SIG_BLOCK, &mask, &prev);
         if(!out){
             debug("solved canceled");
             out=malloc(sizeof(struct result));
